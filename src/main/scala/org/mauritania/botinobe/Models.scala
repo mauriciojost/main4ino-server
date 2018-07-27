5f129ce11ac0a700f8d8dc799a358acdb43c5049
Mauricio Jost <mauricio.jost@gmail.com>
2018-07-27 20:20:40 +0200

    Refactoring

--- src/main/scala/org/mauritania/botinobe/Models.scala
+++ src/main/scala/org/mauritania/botinobe/Models.scala
@@ -1,8 +1,11 @@
 package org.mauritania.botinobe
 
+import org.mauritania.botinobe.Models.Prop.Prop1
+import org.mauritania.botinobe.Models.Target.Target1
+
 object Models {
 
-  // Main types
+  // Basic types
   type RecordId = Long
 
   type DeviceName = String
@@ -14,33 +17,29 @@ object Models {
   type PropsMap = Map[PropName, PropValue]
   type ActorPropsMap = Map[ActorName, PropsMap]
 
-
   type TargetStatus = String
   val Created = "created"
   val Read = "read"
 
+  // Composed types
+
+  /**
+    * Represents a set of properties for
+    * different actors
+    * @param targetId
+    * @param prop1
+    */
   case class Prop(
     targetId: RecordId,
     prop1: Prop1
   )
-
-  case class Prop1(
-    actor: ActorName,
-    prop: PropName,
-    value: PropValue
-  ) {
-    //def asTarget: Target = Target(status, device, Map[ActorName, PropsMap](actor -> Map[PropName, PropValue](prop -> value)))
+  object Prop {
+    case class Prop1(
+      actor: ActorName,
+      prop: PropName,
+      value: PropValue
+    )
   }
-
-  case class Target1 (
-    status: TargetStatus,
-    device: DeviceName
-  )
-
-  case class TargetI (
-    id: RecordId,
-    target: Target
-  )
 
   case class Target(
     target1: Target1,
@@ -53,11 +52,16 @@ object Models {
       } yield (Prop1(actor, propName, propValue))
   }
   object Target {
+
+    case class Target1 (
+      status: TargetStatus,
+      device: DeviceName
+    )
+
     def fromListOfProps(target1: Target1, l: List[Prop1]): Target = {
       val byActor: Map[ActorName, List[Prop1]] = l.groupBy(_.actor)
       val props: ActorPropsMap = byActor.mapValues(_.map(i => (i.value, i.prop)).toMap)
       Target(target1, props)
-
     }
   }
 
