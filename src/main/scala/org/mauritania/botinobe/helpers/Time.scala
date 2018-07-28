c6692c8f57c75ccc6014ea26fc040655ef8baaf9
Mauricio Jost <mauricio.jost@gmail.com>
2018-07-28 18:56:56 +0200

    Timestamps from h2 not working

--- src/main/scala/org/mauritania/botinobe/helpers/Time.scala
+++ src/main/scala/org/mauritania/botinobe/helpers/Time.scala
@@ -1,12 +1,9 @@
 package org.mauritania.botinobe.helpers
 
-import org.h2.util.DateTimeUtils
-
 object Time {
 
 	type Timestamp = Long
 
-	def now: Timestamp = DateTimeUtils.getCalendar.getTimeInMillis
-
+	def now: Timestamp = System.currentTimeMillis()
 
 }
