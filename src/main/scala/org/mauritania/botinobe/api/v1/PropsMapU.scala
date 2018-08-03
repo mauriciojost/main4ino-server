fdca262e8e8d97b7c9601afde02bf94009420634
Mauricio Jost <mauricio.jost@gmail.com>
2018-08-03 04:30:33 +0200

    Cannot reduce empty collection

--- src/main/scala/org/mauritania/botinobe/api/v1/PropsMapU.scala
+++ src/main/scala/org/mauritania/botinobe/api/v1/PropsMapU.scala
@@ -7,7 +7,7 @@ object PropsMapU {
 
   type PropsMapU = Map[PropName, PropValue]
 
-  def fromActorMapU(m: ActorMapU): PropsMapU = m.map(_._2).reduce(_++_) // assumes all properties belong to only one actor
+  def fromActorMapU(m: ActorMapU): PropsMapU = m.map(_._2).fold(Map.empty[PropName, PropValue])(_++_) // assumes all properties belong to only one actor
 
   def fromTups(ps: Iterable[ActorTup]): PropsMapU = fromActorMapU(ActorMapU.fromTups(ps))
 
