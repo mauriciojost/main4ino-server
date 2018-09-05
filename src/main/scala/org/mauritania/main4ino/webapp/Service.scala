bf7e2910eefeb6f5f62425e021ea266b16571156
Mauricio Jost <mauricio.jost@gmail.com>
2018-09-05 17:53:07 +0200

    Redirect root to index.html

--- src/main/scala/org/mauritania/main4ino/webapp/Service.scala
+++ src/main/scala/org/mauritania/main4ino/webapp/Service.scala
@@ -1,22 +1,25 @@
 package org.mauritania.main4ino.webapp
 
+import java.io.File
+
 import cats.effect.IO
-import org.http4s.{Request, Response}
+import org.http4s.{HttpService, Request, Response, StaticFile}
 import org.http4s.dsl.Http4sDsl
-import org.http4s.HttpService
 import org.http4s.server.staticcontent
 import org.http4s.server.staticcontent.ResourceService.Config
 
 class Service() extends Http4sDsl[IO] {
 
-
-  private val static = staticcontent.resourceService[IO](Config(basePath = "/webapp", pathPrefix = "/", cacheStrategy = staticcontent.MemoryCache()))
+  private val IndexHtml = "/webapp/index.html"
+  private val StaticResource = staticcontent.resourceService[IO](Config(basePath = "/webapp", pathPrefix = "/", cacheStrategy = staticcontent.MemoryCache()))
 
   val service = HttpService[IO] {
 
-      case a@GET -> _ => {
-        static(a).value.map(_.getOrElse(Response.notFound[IO]))
-      }
+      case a@GET -> Root =>
+        StaticFile.fromResource(IndexHtml, Some(a)).getOrElseF(InternalServerError())
+
+      case a@GET -> _ =>
+        StaticResource(a).value.map(_.getOrElse(Response.notFound[IO]))
 
     }
 
