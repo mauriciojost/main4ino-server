ebd371512de4010a4360b8207f909ae61342a984
Mauricio Jost <mauricio.jost@gmail.com>
2019-01-25 18:07:06 +0100

    Fix tests

--- src/test/scala/org/mauritania/main4ino/security/ConfigSpec.scala
+++ src/test/scala/org/mauritania/main4ino/security/ConfigSpec.scala
@@ -4,26 +4,25 @@ import java.io.File
 
 import com.typesafe.config.ConfigException
 import org.scalatest._
-import pureconfig.error.ConfigReaderException
 
 class ConfigSpec extends FlatSpec with Matchers {
 
   val User1 = Fixtures.User1
 
   "The security config" should "load correctly a configuration file" in {
-    val c = Config.load(new File("security-users-single.conf")).unsafeRunSync()
+    val c = Config.load(new File("src/test/resources/security-users-single.conf")).unsafeRunSync()
     c.users shouldBe List(User1)
   }
 
   it should "throw an exception if the config is invalid" in {
-    a [ConfigReaderException[Config]] should be thrownBy {
-      Config.load(new File("security-users-invalid.conf")).unsafeRunSync()
+    a [RuntimeException] should be thrownBy {
+      Config.load(new File("src/test/resources/security-users-invalid.conf")).unsafeRunSync()
     }
   }
 
   it should "throw an exception if the config is malformed" in {
     a [ConfigException.Parse] should be thrownBy {
-      Config.load(new File("security-users-broken.conf")).unsafeRunSync()
+      Config.load(new File("src/test/resources/security-users-broken.conf")).unsafeRunSync()
     }
   }
 
