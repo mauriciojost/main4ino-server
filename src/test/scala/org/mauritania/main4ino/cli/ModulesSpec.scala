83d4195874c11722b34b69a67d02a1f04e0bf423
Mauricio Jost <mauricio.jost@gmail.com>
2019-02-23 13:22:31 +0100

    Add tests on filesystem

--- src/test/scala/org/mauritania/main4ino/cli/ModulesSpec.scala
+++ src/test/scala/org/mauritania/main4ino/cli/ModulesSpec.scala
@@ -1,9 +1,11 @@
 package org.mauritania.main4ino.cli
 
+import java.nio.file.{Files, Paths}
+
 import cats.Id
 import org.mauritania.main4ino.SyncId
 import org.mauritania.main4ino.cli.Actions.AddRawUser
-import org.mauritania.main4ino.cli.Modules.ConfigsAppErr
+import org.mauritania.main4ino.cli.Modules.{ConfigsAppErr, FilesystemSync}
 import org.scalatest.{Matchers, WordSpec}
 import org.mauritania.main4ino.security.Fixtures._
 import org.mauritania.main4ino.security.{Authentication, User}
@@ -12,7 +14,7 @@ class ModulesSpec extends WordSpec with Matchers with SyncId {
 
   "A config handler" should {
 
-    "add a user correctly" in {
+    "add a user" in {
       val c = new ConfigsAppErr[Id]()
       val baseConfig = DefaultSecurityConfig
       val newUser = AddRawUser("pepe", "toto", "pepe@zzz.com", List("/"))
@@ -27,6 +29,26 @@ class ModulesSpec extends WordSpec with Matchers with SyncId {
 
       newConf shouldBe baseConfig.copy(users = ExpectedNewUserEntry :: baseConfig.users)
 
+    }
+
+  }
+
+  "A filesystem handler" should {
+
+    "read a file" in {
+      val fs = new FilesystemSync[Id]()
+      val p = Paths.get("src", "test", "resources", "atextfile.txt")
+      val content = fs.readFile(p)
+      content shouldBe "file content"
+    }
+
+    "write a file" in {
+      val fs = new FilesystemSync[Id]()
+      val output = Files.createTempFile("write", "tmp")
+      val content = "0123"
+      fs.writeFile(output, content)
+      output.toFile.length() shouldBe content.length
+      output.toFile.deleteOnExit()
     }
 
   }
