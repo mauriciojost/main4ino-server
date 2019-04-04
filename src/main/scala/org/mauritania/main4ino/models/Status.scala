562fb8c1b2acdc8ab126d9e10b6128cbf47b99b5
Mauricio Jost <mauricio.jost@gmail.com>
2019-04-04 09:07:36 +0200

    Minor refactoring

--- src/main/scala/org/mauritania/main4ino/models/Status.scala
+++
@@ -1,8 +0,0 @@
-package org.mauritania.main4ino.models
-
-object Status {
-
-  val Created: Status = "C"
-  val Consumed: Status = "X"
-
-}
--- src/main/scala/org/mauritania/main4ino/models/package.scala
+++ src/main/scala/org/mauritania/main4ino/models/package.scala
@@ -4,6 +4,11 @@ package object models {
 
 	type Status = String // TODO must use a proper type
 
+	object Status {
+		val Created: Status = "C"
+		val Consumed: Status = "X"
+	}
+
 	type RecordId = Long // TODO change to RequestId
 
 	type DeviceName = String
