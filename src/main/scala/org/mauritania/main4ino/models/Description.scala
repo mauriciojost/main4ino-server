946f0d1949cbe68ab4cca5c2ddab2c77766827c5
Mauricio Jost <mauricio.jost@gmail.com>
2019-07-18 23:35:00 +0200

    Minor comment improvement

--- src/main/scala/org/mauritania/main4ino/models/Description.scala
+++ src/main/scala/org/mauritania/main4ino/models/Description.scala
@@ -4,7 +4,7 @@ import io.circe.Json
 import org.mauritania.main4ino.models.Description.VersionJson
 
 /**
-  * Description of a device, by actors and its properties
+  * Description of a device: its actors and their properties, examples and explanations
   *
   * This class allows the devices to describe their actors and properties as their firmware evolves,
   * decoupling the update in the FE.
