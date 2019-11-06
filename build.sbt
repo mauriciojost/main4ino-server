c478acaae740fac426e67f700ba3d039def0c1b7
Mauricio Jost <mauricio.jost@gmail.com>
2019-11-06 07:35:15 +0100

    Put dependencies in dedicated file

--- build.sbt
+++ build.sbt
@@ -1,20 +1,7 @@
 val ProjectVersion = "0.1.0-SNAPSHOT"
 
 val ScalaVersion = "2.12.8"
-val Http4sVersion = "0.18.14"
-val Specs2Version = "4.2.0"
-val LogbackVersion = "1.2.3"
-val DoobieVersion = "0.5.2"
-val H2Version = "1.4.192"
-val FlywayVersion = "4.2.0"
-val CirceVersion = "0.9.3"
-val PureConfigVersion = "0.10.2"
-val ScalaTestVersion = "3.0.4"
-val ScalaMockVersion = "4.0.0"
-val log4CatsSlf4jVersion = "0.1.0"
-val slf4jLog4j12Version = "1.8.0-beta2"
-val CryptobitsVersion = "1.2"
-val BcryptVersion = "3.1"
+
 
 lazy val root = (project in file("."))
   .settings(
@@ -23,28 +10,7 @@ lazy val root = (project in file("."))
     version := ProjectVersion,
     scalaVersion := ScalaVersion,
     scalacOptions ++= Seq("-Ypartial-unification"),
-    libraryDependencies ++= Seq(
-      "org.reactormonk" %% "cryptobits" % CryptobitsVersion,
-      "com.github.t3hnar" %% "scala-bcrypt" % BcryptVersion,
-      "org.http4s" %% "http4s-blaze-server" % Http4sVersion,
-      "org.http4s" %% "http4s-circe" % Http4sVersion,
-      "io.circe" %% "circe-core" % CirceVersion,
-      "io.circe" %% "circe-generic" % CirceVersion,
-      "io.circe" %% "circe-parser" % CirceVersion,
-      "io.chrisdavenport" %% "log4cats-slf4j" % log4CatsSlf4jVersion,
-      "org.slf4j" % "slf4j-log4j12" % slf4jLog4j12Version,
-      "org.http4s" %% "http4s-dsl" % Http4sVersion,
-      "org.specs2" %% "specs2-core" % Specs2Version % "test",
-      "org.http4s" %% "http4s-blaze-client" % Http4sVersion % "test",
-      "org.tpolecat" %% "doobie-core" % DoobieVersion,
-      "org.tpolecat" %% "doobie-h2" % DoobieVersion,
-      "org.tpolecat" %% "doobie-hikari" % DoobieVersion,
-      "com.h2database" % "h2" % H2Version,
-      "org.flywaydb" % "flyway-core" % FlywayVersion,
-      "com.github.pureconfig" %% "pureconfig" % PureConfigVersion,
-      "org.scalatest" %% "scalatest" % ScalaTestVersion % "test",
-      "org.scalamock" %% "scalamock" % ScalaMockVersion % "test"
-    ),
+    libraryDependencies ++= Dependencies.Dependencies,
     parallelExecution in Test := false,
 
     coverageMinimum := 95,
---
+++ project/Dependencies.scala
@@ -0,0 +1,44 @@
+import sbt._
+
+object Dependencies {
+
+    object Versions {
+        val Http4sVersion = "0.18.14"
+        val Specs2Version = "4.2.0"
+        val LogbackVersion = "1.2.3"
+        val DoobieVersion = "0.5.2"
+        val H2Version = "1.4.192"
+        val FlywayVersion = "4.2.0"
+        val CirceVersion = "0.9.3"
+        val PureConfigVersion = "0.10.2"
+        val ScalaTestVersion = "3.0.4"
+        val ScalaMockVersion = "4.0.0"
+        val log4CatsSlf4jVersion = "0.1.0"
+        val slf4jLog4j12Version = "1.8.0-beta2"
+        val CryptobitsVersion = "1.2"
+        val BcryptVersion = "3.1"
+    }
+
+    val Dependencies = Seq(
+          "org.reactormonk" %% "cryptobits" % Versions.CryptobitsVersion,
+          "com.github.t3hnar" %% "scala-bcrypt" % Versions.BcryptVersion,
+          "org.http4s" %% "http4s-blaze-server" % Versions.Http4sVersion,
+          "org.http4s" %% "http4s-circe" % Versions.Http4sVersion,
+          "io.circe" %% "circe-core" % Versions.CirceVersion,
+          "io.circe" %% "circe-generic" % Versions.CirceVersion,
+          "io.circe" %% "circe-parser" % Versions.CirceVersion,
+          "io.chrisdavenport" %% "log4cats-slf4j" % Versions.log4CatsSlf4jVersion,
+          "org.slf4j" % "slf4j-log4j12" % Versions.slf4jLog4j12Version,
+          "org.http4s" %% "http4s-dsl" % Versions.Http4sVersion,
+          "org.specs2" %% "specs2-core" % Versions.Specs2Version % "test",
+          "org.http4s" %% "http4s-blaze-client" % Versions.Http4sVersion % "test",
+          "org.tpolecat" %% "doobie-core" % Versions.DoobieVersion,
+          "org.tpolecat" %% "doobie-h2" % Versions.DoobieVersion,
+          "org.tpolecat" %% "doobie-hikari" % Versions.DoobieVersion,
+          "com.h2database" % "h2" % Versions.H2Version,
+          "org.flywaydb" % "flyway-core" % Versions.FlywayVersion,
+          "com.github.pureconfig" %% "pureconfig" % Versions.PureConfigVersion,
+          "org.scalatest" %% "scalatest" % Versions.ScalaTestVersion % "test",
+          "org.scalamock" %% "scalamock" % Versions.ScalaMockVersion % "test"
+        )
+}
