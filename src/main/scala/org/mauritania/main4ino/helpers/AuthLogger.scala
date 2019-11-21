dc7a098cf54556c9da0ac508162180a4420a4aac
Mauricio Jost <mauricio.jost@gmail.com>
2019-11-21 22:47:12 +0100

    Authentication logs difference on login failure

--- src/main/scala/org/mauritania/main4ino/helpers/AuthLogger.scala
+++ src/main/scala/org/mauritania/main4ino/helpers/AuthLogger.scala
@@ -10,11 +10,10 @@ object AuthLogger {
   def logAuthentication[F[_]: Sync](user: AccessAttempt): F[AccessAttempt] = {
     for {
       logger <- Slf4jLogger.fromClass[F](getClass)
-      msg = user match {
-        case Right(i) => s">>> Authenticated: ${i.name} ${i.email}"
-        case Left(m) => s">>> Failed to authenticate: $m"
+      _ <- user match {
+        case Right(i) => logger.debug(s">>> Authenticated: ${i.name}/${i.email}")
+        case Left(m) => logger.info(s">>> Failed to authenticate: $m")
       }
-      _ <- logger.debug(msg)
     } yield (user)
   }
 
