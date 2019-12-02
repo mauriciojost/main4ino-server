296d88ec42678153dae680c645a62f75e5527c94
Mauricio Jost <mauricio.jost@gmail.com>
2019-12-02 13:40:24 +0100

    Switch to varchars whenever text

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,15 +1,15 @@
 CREATE TABLE targets_requests (
   id SERIAL PRIMARY KEY,
   creation LONG,
-  device_name TEXT,
-  status TEXT
+  device_name VARCHAR(20),
+  status VARCHAR(1)
 );
 
 CREATE TABLE targets (
   id SERIAL PRIMARY KEY,
   request_id INT,
-  actor_name TEXT,
-  property_name TEXT,
+  actor_name VARCHAR(20),
+  property_name VARCHAR(20),
   property_value TEXT,
   creation LONG
 );
@@ -17,30 +17,30 @@ CREATE TABLE targets (
 CREATE TABLE reports_requests (
   id SERIAL PRIMARY KEY,
   creation LONG,
-  device_name TEXT,
-  status TEXT
+  device_name VARCHAR(20),
+  status VARCHAR(1)
 );
 
 CREATE TABLE reports (
   id SERIAL PRIMARY KEY,
   request_id INT,
-  actor_name TEXT,
-  property_name TEXT,
+  actor_name VARCHAR(20),
+  property_name VARCHAR(20),
   property_value TEXT,
   creation LONG
 );
 
 CREATE TABLE descriptions (
   id SERIAL PRIMARY KEY,
-  device_name TEXT,
+  device_name VARCHAR(20),
   updated LONG,
   version TEXT,
   json TEXT
 );
 
-CREATE INDEX targets_requests_id_index ON targets_requests (device_name, status, id);
+CREATE INDEX targets_requests_id_index ON targets_requests (device_name,status,id);
 CREATE INDEX targets_request_id_index ON targets (request_id);
 
-CREATE INDEX reports_requests_id_index ON reports_requests (device_name, status, id);
+CREATE INDEX reports_requests_id_index ON reports_requests (device_name,status,id);
 CREATE INDEX reports_request_id_index ON reports (request_id);
 
