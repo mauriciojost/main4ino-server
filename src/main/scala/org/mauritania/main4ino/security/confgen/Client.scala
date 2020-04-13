1c4d4adc3857263650eb59b307c8eae7115acf49
Mauricio Jost <mauriciojost@gmail.com>
2020-04-13 18:39:42 +0200

    confgen.Client more verbose

--- src/main/scala/org/mauritania/main4ino/security/confgen/Client.scala
+++ src/main/scala/org/mauritania/main4ino/security/confgen/Client.scala
@@ -3,6 +3,7 @@ package org.mauritania.main4ino.security.confgen
 import cats.effect.{IO, Sync}
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
 import org.mauritania.main4ino.security.confgen.Algebras.{Configs, Filesystem}
 import org.mauritania.main4ino.security.confgen.Modules.{ConfigsMonad, FilesystemSync}
 import org.mauritania.main4ino.helpers.ConfigLoader
@@ -16,9 +17,13 @@ object Client {
 
   def start[F[_]: Sync](O: Configs[F], S: Filesystem[F]): F[Unit] = {
     for {
+      logger <- Slf4jLogger.fromClass[F](getClass)
       args <- ConfigLoader.fromEnv[F, Args]
+      _ <- logger.debug(s"Args: $args")
       conf <- ConfigLoader.fromFile[F, Config](args.input.toFile)
+      _ <- logger.debug(s"Configuration: $conf")
       actions <- ConfigLoader.fromFile[F, Actions](args.modif.toFile)
+      _ <- logger.debug(s"Actions: $actions")
       newConf <- O.performActions(conf, actions.merged)
       newConfStr = O.asString(newConf)
       _ <- S.writeFile(args.output, newConfStr)
