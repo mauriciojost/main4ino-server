7ec557d8c33b5cafe783e802719ba246f3ad923a
Scala Steward <me@scala-steward.org>
2020-04-22 10:32:52 +0200

    Update refined, refined-pureconfig to 0.9.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
         val CryptobitsVersion = "1.3"
         val GfcSemverVersion = "0.0.5"
         val EnumeratumVersion = "1.5.15"
-        val RefinedVersion = "0.9.13"
+        val RefinedVersion = "0.9.14"
         val GfcSemver = "0.0.5"
         val TsecVersion = "0.2.0-RC1"
     }
