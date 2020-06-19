4b1127996fbfb2f885d2a6eb4111257d188442d5
Mauricio Jost <mauriciojost@gmail.com>
2020-06-19 12:42:03 +0200

    Faster partitioners

--- src/main/scala/org/mauritania/main4ino/devicelogs/Partitioner.scala
+++ src/main/scala/org/mauritania/main4ino/devicelogs/Partitioner.scala
@@ -1,10 +1,10 @@
 package org.mauritania.main4ino.devicelogs
 
 import java.time.format.DateTimeFormatter
-import java.time.{Instant, ZoneOffset}
+import java.time.{Duration, Instant, ZoneOffset}
 
 import org.mauritania.main4ino.devicelogs.Partitioner.EpochSecPartitioner.partition
-import org.mauritania.main4ino.devicelogs.Partitioner.HourPartitioner.partition
+import org.mauritania.main4ino.devicelogs.Partitioner.HourPartitioner.{SecsInHour, partition}
 import org.mauritania.main4ino.models.EpochSecTimestamp
 
 object Partitioner {
@@ -24,15 +24,17 @@ object Partitioner {
   }
 
   case object HourPartitioner extends Partitioner {
+    private final val SecsInHour = 60 * 60
     private val format = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH")
     def partition(r: RecordId): Partition = Instant.ofEpochSecond(r).atZone(ZoneOffset.UTC).format(format)
-    def partitions(f: RecordId, t: RecordId): Set[Partition] = (f to t).map(partition).toSet
+    def partitions(f: RecordId, t: RecordId): Set[Partition] = (f.to(t, SecsInHour)).map(partition).toSet
   }
 
   case object DayPartitioner extends Partitioner {
+    private final val SecsInDay = 60 * 60 * 24
     private val format = DateTimeFormatter.ofPattern("yyyy-MM-dd")
     def partition(r: RecordId): Partition = Instant.ofEpochSecond(r).atZone(ZoneOffset.UTC).format(format)
-    def partitions(f: RecordId, t: RecordId): Set[Partition] = (f to t).map(partition).toSet
+    def partitions(f: RecordId, t: RecordId): Set[Partition] = (f.to(t, SecsInDay)).map(partition).toSet
   }
 
 }
