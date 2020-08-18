54b3ba7d1f70467a6ef04eb3e5f7c5bf04b71397
Scala Steward <me@scala-steward.org>
2020-08-18 14:36:44 +0200

    Update flyway-core to 6.5.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
         val DoobieVersion = "0.9.0"
         val H2Version = "1.4.200"
         val SqliteVersion = "3.32.3.1"
-        val FlywayVersion = "6.5.4"
+        val FlywayVersion = "6.5.5"
         val CirceVersion = "0.13.0"
         val PureConfigVersion = "0.13.0"
         val ScalaTestVersion = "3.2.2"
