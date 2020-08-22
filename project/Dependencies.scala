795d819a76f5092ff82de3294ae3bd410df27a8f
Scala Steward <me@scala-steward.org>
2020-08-22 07:33:52 +0200

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
         val FlywayVersion = "6.5.4"
         val CirceVersion = "0.13.0"
         val PureConfigVersion = "0.13.0"
-        val ScalaTestVersion = "3.2.1"
+        val ScalaTestVersion = "3.2.2"
         val ScalaMockVersion = "5.0.0"
         val FreePortFinder = "1.1.1"
         val log4CatsSlf4jVersion = "1.1.1"
