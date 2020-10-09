4fbebb6c30013f9ace02e16550b3fb0a9c68d54f
Scala Steward <me@scala-steward.org>
2020-10-09 15:02:58 +0200

    Update specs2-core to 4.10.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
     object Versions {
         val Http4sVersion = "0.21.7"
-        val Specs2Version = "4.10.3"
+        val Specs2Version = "4.10.4"
         val DoobieVersion = "0.9.2"
         val H2Version = "1.4.200"
         val SqliteVersion = "3.32.3.1"
