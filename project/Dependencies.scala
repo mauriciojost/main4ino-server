d5a42cb09001e532d2a9ee87e28ad6475d540a7b
Scala Steward <me@scala-steward.org>
2020-12-06 10:27:41 +0100

    Update scalamock to 5.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
         val CirceVersion = "0.13.0"
         val PureConfigVersion = "0.14.0"
         val ScalaTestVersion = "3.2.3"
-        val ScalaMockVersion = "5.0.0"
+        val ScalaMockVersion = "5.1.0"
         val FreePortFinder = "1.1.1"
         val log4CatsSlf4jVersion = "1.1.1"
         val slf4jLog4j12Version = "1.7.30"
