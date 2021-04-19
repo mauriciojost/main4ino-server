3f97c237a349802834a9442aca374e62972a535b
Scala Steward <me@scala-steward.org>
2021-04-19 22:31:18 +0200

    Update doobie-core, doobie-h2, ... to 0.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     object Versions {
         val Http4sVersion = "0.21.20"
         val Specs2Version = "4.10.6"
-        val DoobieVersion = "0.12.1"
+        val DoobieVersion = "0.13.0"
         val H2Version = "1.4.200"
         val SqliteVersion = "3.34.0"
         val FlywayVersion = "7.5.4"
