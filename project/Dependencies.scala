fe8da826133d9da83e1e2676da37e7626881ca62
Scala Steward <me@scala-steward.org>
2021-07-20 15:31:35 +0200

    Update slf4j-log4j12 to 1.7.32

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
         val ScalaMockVersion = "5.1.0"
         val FreePortFinder = "1.1.1"
         val log4CatsSlf4jVersion = "1.3.1"
-        val slf4jLog4j12Version = "1.7.30"
+        val slf4jLog4j12Version = "1.7.32"
         val CryptobitsVersion = "1.3"
         val GfcSemverVersion = "0.0.5"
         val EnumeratumVersion = "1.7.0"
