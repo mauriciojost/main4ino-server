bb270b9c7183c1bd65b8eabdb33af6f26bb07c56
Scala Steward <me@scala-steward.org>
2021-11-30 03:53:53 +0100

    Update h2 to 2.0.202

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
         val Http4sVersion = "0.21.31"
         val Specs2Version = "4.11.0"
         val DoobieVersion = "0.13.4"
-        val H2Version = "1.4.200"
+        val H2Version = "2.0.202"
         val SqliteVersion = "3.36.0"
         val FlywayVersion = "8.0.4"
         val CirceVersion = "0.14.1"
